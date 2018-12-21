ccae60a90899a3f3132693c7ca38cb467702a0c8
Tamas Szirbucz <tamas_szirbucz@epam.com>
2018-12-21 15:05:21 +0100

    initial commit

---
+++ precache-manifest.70aff13c55c1706ba20aea64e87d54ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da4da52a1a5e9a39db8d",
+    "url": "/static/css/main.ce3986c3.chunk.css"
+  },
+  {
+    "revision": "da4da52a1a5e9a39db8d",
+    "url": "/static/js/main.da4da52a.chunk.js"
+  },
+  {
+    "revision": "d8dd441c360463fc05d8",
+    "url": "/static/js/1.d8dd441c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1afeff7593017ebeadc73e6a8a9c3b27",
+    "url": "/index.html"
+  }
+];
