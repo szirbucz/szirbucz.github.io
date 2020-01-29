b3d89fde17e1d2af71eaf2fd526178fb7f44376a
Tamas Szirbucz <szirbucz@kodekonveyor.com>
2020-01-29 09:56:13 +0100

    Change to my CV

---
+++ precache-manifest.5c8f3accd4339e0cf4f1d1e212fcfbad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9dfc3a3177838f6c28a9feb968f2fc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d483f5b2cac43f9ea20",
+    "url": "/static/css/main.ecbdd6fe.chunk.css"
+  },
+  {
+    "revision": "930ba82d0ff062c13ab3",
+    "url": "/static/js/2.de2aaca0.chunk.js"
+  },
+  {
+    "revision": "0bca0224b6af2a656dcc680de3c649da",
+    "url": "/static/js/2.de2aaca0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1d483f5b2cac43f9ea20",
+    "url": "/static/js/main.4f99fd65.chunk.js"
+  },
+  {
+    "revision": "b2fb3e6d001ecef7364f",
+    "url": "/static/js/runtime-main.a1a920d6.js"
+  }
+]);
